0863b7684e0b848bbdd2a4788d0a1bd76ce4c65c
Seva <sevauni@gmail.com>
2023-01-04 23:17:38 +0200

    numbers validation

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// const inputField = document.querySelector("#number");
+
+// inputField.addEventListener('input', () => {
+//     console.log("test");
+//     return false;
+// });
