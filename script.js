de886e497812748ba197f498cdc03345d1d7492b
Seva <sevauni@gmail.com>
2023-01-05 10:14:23 +0200

    release

--- script.js
+++ script.js
@@ -1,12 +1,61 @@
 "use strict";
 
-// const inputField = document.querySelector("#number");
 
-// inputField.addEventListener('input', () => {
-//     console.log("test");
-//     return false;
-// });
+const searchButton = document.querySelector(".inputbox-text-button");
+const inputElement = document.querySelector(".inputbox");
+const resultText = document.querySelector(".result-prime");
+const inputBox = document.querySelector("#number");
+const buttonImg = document.querySelector("#search-button");
+let ready = false;
 
 
-const inputField = document.querySelector("#number");
-console.log(inputField.getBoundingClientRect(inputField));
+console.log(window.getComputedStyle(document.documentElement).getPropertyValue('--number-element-final-size')); // #336699
+
+
+
+
+document.body.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        if (ready) {
+            return;
+        }
+        checkHandler();
+    }
+});
+
+searchButton.addEventListener('click', () => {
+    if (ready) {
+        return;
+    }
+    checkHandler();
+});
+
+
+
+function checkHandler() {
+    ready = true;
+    if (inputBox.value.length === 0) {
+        return;
+    }
+
+    if (isPrime(parseInt(inputBox.value))) {
+        resultText.innerHTML = 'Your number is prime!';
+    } else {
+        resultText.innerHTML = 'Your number is not prime!';
+    }
+
+    inputBox.disabled = true;
+    document.documentElement.style.setProperty('--number-element-final-size', `${inputBox.value.length * 45 + 80}px`);
+    searchButton.classList.add("inputbox-text-button-play");
+    inputElement.classList.add("inputbox-play");
+    resultText.classList.add("result-prime-play");
+}
+
+function isPrime(num) {
+    for (let i = 2, s = Math.sqrt(num); i <= s; i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return num > 1;
+}
